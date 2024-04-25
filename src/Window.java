5da58f76f4bc8aaa8b3c1561e1c697176067a497
marcingarcin <marcin.galazka@o465.us.edu.pl>
2024-04-26 01:06:09 +0200

    Created simple login window

--- src/Window.java
+++ src/Window.java
@@ -2,17 +2,31 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Window extends JFrame {
-    private Color bg = new Color(51,51,51);
+    private Color grey = new Color(51, 51, 51);
+    private Color darkerGrey = new Color(40, 40, 40);
+    private int width = 1280;
+    private int height = 720;
+
     public Window() {
         setupWindow();
+        setupJoinGamePanel();
     }
-    public void setupWindow(){
-        setSize(1280,720);
+
+    public void setupWindow() {
+        setSize(width, height);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
         setVisible(true);
-        setBackground(Color.RED);
+        getContentPane().setBackground(grey);
+    }
+
+    public void setupJoinGamePanel() {
+        JPanel joinGamePanel = new JPanel();
+        joinGamePanel.setBackground(Color.red);
+        joinGamePanel.setBounds(width/2 - width/6,height/2 - height/3, width/6,height/3);
 
 
+        add(joinGamePanel);
     }
 }
