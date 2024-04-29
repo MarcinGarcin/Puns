d6bbf29a17b66bfcac97b8b485ee2a5e16e738d2
marcingarcin <knucias123@gmail.com>
2024-04-30 00:12:45 +0200

    Literally my brain is ded

--- Server/ClientHandler.java
+++ Server/ClientHandler.java
@@ -2,29 +2,32 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
-import java.util.ArrayList;
 
-class ClientHandler implements Runnable {
+public class ClientHandler extends Thread {
     private Socket clientSocket;
-    private Player player;
     private ObjectOutputStream out;
     private ObjectInputStream in;
-    ArrayList<Player> players;
 
-    public ClientHandler(Socket clientSocket, Player player, ArrayList<Player> players) {
-        this.clientSocket = clientSocket;
-        this.player = player;
-        this.players = players;
-        try {
-            this.out = new ObjectOutputStream(clientSocket.getOutputStream());
-            this.in = new ObjectInputStream(clientSocket.getInputStream());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public ClientHandler(Socket socket) {
+        this.clientSocket = socket;
     }
 
-    @Override
     public void run() {
+        try {
+            out = new ObjectOutputStream(clientSocket.getOutputStream());
+            in = new ObjectInputStream(clientSocket.getInputStream());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        try {
+            in.close();
+            out.close();
+            clientSocket.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
-
--- Server/Server.java
+++ Server/Server.java
@@ -1,66 +1,27 @@
-import java.io.*;
-import java.net.*;
-import java.util.ArrayList;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.io.IOException;
+import java.net.ServerSocket;
+
 
 public class Server {
     private ServerSocket serverSocket;
-    private ExecutorService executorService;
-    private ArrayList<Player> players;
-    private ArrayList<Socket> sockets;
 
-    public Server(int port) {
-        try {
-            serverSocket = new ServerSocket(port);
-            executorService = Executors.newFixedThreadPool(10);
-            players = new ArrayList<>();
-            sockets = new ArrayList<>();
-            System.out.println("Server started. Waiting for clients...");
-        } catch (IOException e) {
-            e.printStackTrace();
+
+    public void start(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+        while (true){
+            new ClientHandler(serverSocket.accept()).start();
+            System.out.println("new client connected");
         }
+
     }
 
-    public void startServer() {
-        try {
-            while (true) {
-                Socket clientSocket = serverSocket.accept();
-                executorService.execute(() -> handleClient(clientSocket));
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void stop() throws IOException {
+        serverSocket.close();
     }
 
-    private void handleClient(Socket clientSocket) {
-        try {
-            ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream());
-            Object object = in.readObject();
-
-            if (object instanceof Player) {
-                Player player = (Player) object;
-                System.out.println(player.getName() + " has joined the game.");
-
-                synchronized (players) {
-                    players.add(player);
-                }
-                synchronized (sockets){
-                    sockets.add(clientSocket);
-                }
-                for(Player p : players) {
-                    System.out.println(p.getName());
-                }
-            } else {
-                System.out.println("Received object is not a Player object.");
-            }
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void main(String[] args) {
-        Server server = new Server(8888);
-        server.startServer();
+    public static void main(String[] args) throws IOException {
+        Server server = new Server();
+        server.start(8888);
     }
 }
+
